cd0054c279f067515eb590d7e43f450fb6549587
tru-ten <74614544+tru-ten@users.noreply.github.com>
2023-05-10 04:15:07 +0300

    Update setup.py

---
+++ clean_folder/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='clean_folder',
+      version='1',
+      description='Will clean your dirty folder :)',
+      url='https://github.com/tru-ten/Module7',
+      author='Bohdan Zhemelko',
+      author_email='bogdan.zhemelko@gmail.com',
+      license='lol',
+      packages=['clean_folder'])
