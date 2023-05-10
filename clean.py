78807fda8b7fc68658ec7a85d753ce11e3c62d28
tru-ten <74614544+tru-ten@users.noreply.github.com>
2023-05-10 03:39:43 +0300

    combining previous modules into one

---
+++ clean.py
@@ -0,0 +1,186 @@
+import re
+import sys
+from pathlib import Path
+import shutil
+
+CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", 'i', "ji", "g")
+
+TRANS = {}
+
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
+
+def normalize(name: str) -> str:
+    t_name = name.translate(TRANS)
+    t_name = re.sub(r'\W', '.', t_name)
+    return t_name
+
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+AVI_VIDEO = []
+MP4_VIDEO = []
+MOV_VIDEO = []
+MKV_VIDEO = []
+DOC_DOCUMENTS = []
+DOCX_DOCUMENTS = []
+TXT_DOCUMENTS = []
+PDF_DOCUMENTS = []
+XLSX_DOCUMENTS = []
+PPTX_DOCUMENTS = []
+MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
+AMR_AUDIO = []
+ZIP_ARCHIVES = []
+GZ_ARCHIVES = []
+TAR_ARCHIVES = []
+OTHER = []  #TODO: Can be a problem!!!
+
+
+REGISTER_EXTENSION = {
+    'JPEG': JPEG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'MP3': MP3_AUDIO,
+    'AVI': AVI_VIDEO,
+    'MP4': MP4_VIDEO,
+    'MOV': MOV_VIDEO,
+    'MKV': MKV_VIDEO,
+    'DOC': DOC_DOCUMENTS,
+    'DOCX': DOCX_DOCUMENTS,
+    'TXT': TXT_DOCUMENTS,
+    'PDF': PDF_DOCUMENTS,
+    'XLSX': XLSX_DOCUMENTS,
+    'PPTX': PPTX_DOCUMENTS,
+    'OGG': OGG_AUDIO,
+    'WAV': WAV_AUDIO,
+    'AMR': AMR_AUDIO,
+    'ZIP': ZIP_ARCHIVES,
+    'GZ': GZ_ARCHIVES,
+    'TAR': TAR_ARCHIVES
+}
+
+FOLDERS = []
+EXTENSIONS = set()
+UNKNOWN = set()
+
+
+def get_extension(filename: str) -> str:
+    return Path(filename).suffix[1:].upper()
+
+
+def scan(folder: Path):
+    for item in folder.iterdir():
+        # Робота з папкою
+        if item.is_dir():
+            # Перевіряємо, щоб папка не була тією в яку ми вже складаємо файли.
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'other'):
+                FOLDERS.append(item)
+                scan(item)  # скануємо цю вкладену папку - рекурсія
+            continue  # переходимо до наступного елемента в сканованій папці
+
+        # Робота з файлом
+        ext = get_extension(item.name)  # беремо розширення файлу
+        full_name = folder / item.name  # беремо повний шлях до файлу
+        if not ext:
+            OTHER.append(full_name)
+        else:
+            try:
+                container = REGISTER_EXTENSION[ext]
+                EXTENSIONS.add(ext)
+                container.append(full_name)
+            except KeyError:
+                UNKNOWN.add(ext)
+                OTHER.append(full_name)
+
+def handle_media(filename: Path, target_folder: Path) -> None:
+    target_folder.mkdir(exist_ok=True, parents=True)
+    filename.replace(target_folder / normalize(filename.name))
+
+def handle_other(filename: Path, target_folder: Path) -> None:
+    target_folder.mkdir(exist_ok=True, parents=True)
+    filename.replace(target_folder / normalize(filename.name))
+
+def handle_archive(filename: Path, target_folder: Path) -> None:
+    target_folder.mkdir(exist_ok=True, parents=True)
+    folder_for_file = target_folder / normalize(filename.name.replace(filename.suffix, ''))
+    folder_for_file.mkdir(exist_ok=True, parents=True)
+    try:
+        shutil.unpack_archive(str(filename.resolve()), str(folder_for_file.resolve()))
+    except shutil.ReadError:
+        folder_for_file.rmdir()
+        return None
+    filename.unlink()
+
+def handle_folder(folder: Path) -> None:
+    try:
+        folder.rmdir()
+    except OSError:
+        print(f'Sorry, we can not delete the folder: {folder}')
+
+
+def main(folder: Path) -> None:
+    scan(folder)
+    for file in JPEG_IMAGES:
+        handle_media(file, folder / 'images' / 'JPEG')
+    for file in JPG_IMAGES:
+        handle_media(file, folder / 'images' / 'JPG')
+    for file in PNG_IMAGES:
+        handle_media(file, folder / 'images' / 'PNG')
+    for file in SVG_IMAGES:
+        handle_media(file, folder / 'images' / 'SVG')
+
+    for file in AVI_VIDEO:
+        handle_media(file, folder / 'video' / 'AVI')
+    for file in MP4_VIDEO:
+        handle_media(file, folder / 'video' / 'MP4')
+    for file in MOV_VIDEO:
+        handle_media(file, folder / 'video' / 'MOV')
+    for file in MKV_VIDEO:
+        handle_media(file, folder / 'video' / 'MKV')
+
+    for file in DOC_DOCUMENTS:
+        handle_other(file, folder / 'documents' / 'DOC')
+    for file in DOCX_DOCUMENTS:
+        handle_other(file, folder / 'documents' / 'DOCX')
+    for file in TXT_DOCUMENTS:
+        handle_other(file, folder / 'documents' / 'TXT')
+    for file in PDF_DOCUMENTS:
+        handle_other(file, folder / 'documents' / 'PDF')
+    for file in XLSX_DOCUMENTS:
+        handle_other(file, folder / 'documents' / 'XLSX')
+    for file in PPTX_DOCUMENTS:
+        handle_other(file, folder / 'documents' / 'PPTX')
+
+    for file in MP3_AUDIO:
+        handle_media(file, folder / 'audio' / 'MP3')
+    for file in OGG_AUDIO:
+        handle_media(file, folder / 'audio' / 'OGG')
+    for file in WAV_AUDIO:
+        handle_media(file, folder / 'audio' / 'WAV')
+    for file in AMR_AUDIO:
+        handle_media(file, folder / 'audio' / 'AMR')
+
+    for file in OTHER:
+        handle_other(file, folder / 'Other')
+
+    for file in ZIP_ARCHIVES:
+        handle_archive(file, folder / 'archives' / 'ZIP')
+    for file in GZ_ARCHIVES:
+        handle_archive(file, folder / 'archives' / 'GZ')
+    for file in TAR_ARCHIVES:
+        handle_archive(file, folder / 'archives' / 'TAR')
+
+    for folder in FOLDERS[::-1]:
+        handle_folder(folder)
+
+
+if __name__ == '__main__':
+    folder_for_scan = Path(sys.argv[1])
+    main(folder_for_scan.resolve())
